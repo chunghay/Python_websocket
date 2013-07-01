40d94c33191129058ad6eabd9813c6eaf2838b92
Chung-Hay Luk <luk@chunghay.com>
2013-06-30 17:52:40 -0700

    Add licensing

--- myserver2arduino.py
+++ myserver2arduino.py
@@ -1,3 +1,31 @@
+###############################################################################
+## All code excluding the websocket sections was written by Chung-Hay Luk.
+##  Copyright 2013
+##  Licensed under the Apache License, Version 2.0 (the "License").
+##  See below for license details.
+##
+## Websocket sections of the code (classes, creating factory sections) are
+## modified from example code for AutobahnPython found here:
+## https://github.com/tavendo/AutobahnPython/blob/master/examples/websocket/broadcast/server.py
+##
+##  It is licensed as such:
+##
+##  Copyright 2011,2012 Tavendo GmbH
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##
+###############################################################################
+
 import argparse
 import json
 import serial
